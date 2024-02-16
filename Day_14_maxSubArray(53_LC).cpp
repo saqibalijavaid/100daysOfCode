55fd32cea0a2e0780a6bd74ee284892234ec5b48
Saqib Ali Javaid <saqibalijavaid2@gmail.com>
2024-02-16 21:28:06 +0500

    Create Day_14_maxSubArray(53_LC).cpp

---
+++ Day_14_maxSubArray(53_LC).cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        // Kadane's Algo
+        int sum = 0, max = INT_MIN;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            sum += nums[i];
+
+            if (sum > max)
+                max = sum;
+
+            if (sum < 0)
+                sum = 0;
+        }
+
+        return max;
+    }
+};
