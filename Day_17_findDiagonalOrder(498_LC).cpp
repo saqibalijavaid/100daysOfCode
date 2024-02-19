7c4683edb7ea97a47847b1a09d49b48bb82a2a8d
Saqib Ali Javaid <saqibalijavaid2@gmail.com>
2024-02-20 00:36:39 +0500

    Update Day_17_findDiagonalOrder(498_LC).cpp

--- Day_17_findDiagonalOrder(498_LC).cpp
+++ Day_17_findDiagonalOrder(498_LC).cpp
@@ -1,9 +1,57 @@
-#include <iostream>
-using namespace std;
-
-int main()
+class Solution
 {
-    cout << "HelloWorld";
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>> &mat)
+    {
+        int m = mat.size();
+        int n = mat[0].size();
 
-    return 0;
-}
+        int row = 0;
+        int col = 0;
+
+        vector<int> ans;
+
+        bool up = true;
+
+        while (row < m && col < n)
+        {
+            if (up == true)
+            {
+                while (row > 0 && col < n - 1)
+                {
+                    ans.push_back(mat[row][col]);
+                    row--;
+                    col++;
+                }
+
+                ans.push_back(mat[row][col]);
+                
+                if (col == n - 1)
+                    row++;
+                else
+                    col++;
+            }
+
+            else
+            {
+                while (row < m - 1 && col > 0)
+                {
+                    ans.push_back(mat[row][col]);
+                    row++;
+                    col--;
+                }
+
+                ans.push_back(mat[row][col]);
+
+                if (row == m - 1)
+                    col++;
+                else
+                    row++;
+            }
+
+            up =! up;
+        }
+
+        return ans;
+    }
+};
