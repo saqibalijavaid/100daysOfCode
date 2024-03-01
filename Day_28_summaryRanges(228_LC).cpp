eca550176eaed4bd3715d9fdabf1d6b17153c16f
Saqib Ali Javaid <saqibalijavaid2@gmail.com>
2024-03-01 23:57:39 +0500

    Create Day_28_summaryRanges(228_LC).cpp

---
+++ Day_28_summaryRanges(228_LC).cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+public:
+    vector<string> summaryRanges(vector<int> &nums)
+    {
+        vector<string> ans;
+        int first = nums[0];
+        int last = nums[0];
+
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i - 1] + 1 == nums[i])
+                last++;
+            else
+            {
+                if (first == last)
+                    ans.push_back(to_string(first));
+                else if (first != last)
+                    ans.push_back(to_string(first) + "->" + to_string(last));
+
+                first = nums[i];
+                last = nums[i];
+            }
+        }
+
+        // Push the last range
+        if (first == last)
+            ans.push_back(to_string(first));
+        else
+            ans.push_back(to_string(first) + "->" + to_string(last));
+
+        return ans;
+    }
+};
