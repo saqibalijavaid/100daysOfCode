adca40b60d94ff86bef9af9d180b512409010107
Saqib Ali Javaid <saqibalijavaid2@gmail.com>
2024-03-02 22:54:03 +0500

    Create Day_29_nthUglyNumber(264_LC).cpp

---
+++ Day_29_nthUglyNumber(264_LC).cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+
+        vector<int> uglyNumbers(n);
+        uglyNumbers[0] = 1;
+
+        int index2 = 0, index3 = 0, index5 = 0;
+
+        for (int i = 1; i < n; ++i) {
+            int nextUgly = min(uglyNumbers[index2] * 2, min(uglyNumbers[index3] * 3, uglyNumbers[index5] * 5));
+            uglyNumbers[i] = nextUgly;
+
+            if (nextUgly == uglyNumbers[index2] * 2) ++index2;
+            if (nextUgly == uglyNumbers[index3] * 3) ++index3;
+            if (nextUgly == uglyNumbers[index5] * 5) ++index5;
+        }
+
+        return uglyNumbers[n - 1];
+    }
+};
+
+// video: https://youtu.be/Mjnku18C-VM?si=qGvkzoachHSpx98f
