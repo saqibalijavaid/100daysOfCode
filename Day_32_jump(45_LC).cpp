fe4b6e9fba44ac17ec0204e9badf4c2fdb69421b
Saqib Ali Javaid <saqibalijavaid2@gmail.com>
2024-03-06 00:19:25 +0500

    Update Day_32_jump(45_LC).cpp

--- Day_32_jump(45_LC).cpp
+++ Day_32_jump(45_LC).cpp
@@ -1,1 +1,36 @@
-Hello World
+class Solution
+{
+public:
+    int jump(vector<int> &nums)
+    {
+        int totalJumps = 0;
+
+        // destination -> last index
+        int destination = nums.size() - 1;
+        int coverage = 0;
+        int lastJumpIndex = 0;
+
+        // Base Case
+        if (nums.size() == 1)
+            return 0;
+
+        // Greedt Approach
+        for (int i = 0; i < nums.size(); i++)
+        {
+            coverage = max(coverage, i + nums[i]);
+            if (i == lastJumpIndex)
+            {
+                lastJumpIndex = coverage;
+                totalJumps++;
+
+                // check if we reached destination already
+                if (coverage >= destination)
+                {
+                    return totalJumps;
+                }
+            }
+        }
+
+        return totalJumps;
+    }
+};
