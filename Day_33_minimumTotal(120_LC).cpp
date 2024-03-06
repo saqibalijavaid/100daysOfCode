480be4062768a54b661fb4a1d0b089281a4d9cde
Saqib Ali Javaid <saqibalijavaid2@gmail.com>
2024-03-07 00:18:48 +0500

    Update Day_33_minimumTotal(120_LC).cpp

--- Day_33_minimumTotal(120_LC).cpp
+++ Day_33_minimumTotal(120_LC).cpp
@@ -1,1 +1,22 @@
-hello
+class Solution
+{
+public:
+    int minimumTotal(vector<vector<int>> &triangle)
+    {
+        // Bottom-Up Approach
+        int height = triangle.size();
+        vector<vector<int>> dp(height + 1, vector<int>(height + 1, 0));
+
+        for (int level = height - 1; level >= 0; level--)
+        {
+            for (int i = 0; i <= level; i++)
+            {
+                // Add the minimum amongst 2 adjacent elements
+                // from bottom level
+                dp[level][i] = triangle[level][i] + min(dp[level + 1][i], dp[level + 1][i + 1]);
+            }
+        }
+
+        return dp[0][0];
+    }
+};
