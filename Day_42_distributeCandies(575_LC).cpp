bee1d325fc0ef0ca0a39ce5b92df184b5c77e0ac
Saqib Ali Javaid <saqibalijavaid2@gmail.com>
2024-03-16 00:28:03 +0500

    Update Day_42_distributeCandies(575_LC).cpp

--- Day_42_distributeCandies(575_LC).cpp
+++ Day_42_distributeCandies(575_LC).cpp
@@ -1,1 +1,18 @@
-Hello
+class Solution
+{
+public:
+    int distributeCandies(vector<int> &candy)
+    {
+        sort(candy.begin(), candy.end());
+
+        int count = 1;
+        for (int i = 1; i < candy.size(); i++)
+        {
+            if (count == candy.size() / 2)
+                return count;
+            if (candy[i - 1] < candy[i])
+                count++;
+        }
+        return count;
+    }
+};
