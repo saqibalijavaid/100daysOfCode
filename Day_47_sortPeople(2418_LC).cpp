57a4e1292cb4ae4e031beb1849820938d784b2cf
Saqib Ali Javaid <saqibalijavaid2@gmail.com>
2024-03-21 01:12:54 +0500

    Update Day_47_sortPeople(2418_LC).cpp

--- Day_47_sortPeople(2418_LC).cpp
+++ Day_47_sortPeople(2418_LC).cpp
@@ -1,1 +1,19 @@
-hello
+class Solution
+{
+public:
+    vector<string> sortPeople(vector<string> &names, vector<int> &heights)
+    {
+        map<int, string> hash;
+
+        for (int i = 0; i < heights.size(); i++)
+            hash[heights[i]] = names[i];
+
+        sort(heights.rbegin(), heights.rend()); // Descending order
+
+        vector<string> ans;
+        for (int i = 0; i < heights.size(); i++)
+                ans.push_back(hash[heights[i]]);
+
+        return ans;
+    }
+};
