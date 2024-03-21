4a3272b6e71e7d0fb12e5207fb887470aaf347a6
Saqib Ali Javaid <saqibalijavaid2@gmail.com>
2024-03-22 00:31:16 +0500

    Update Day_48_canPlaceFlowers(605_LC).cpp

--- Day_48_canPlaceFlowers(605_LC).cpp
+++ Day_48_canPlaceFlowers(605_LC).cpp
@@ -1,1 +1,29 @@
-hello
+class Solution
+{
+public:
+    bool canPlaceFlowers(vector<int> &nums, int n)
+    {
+        if (n == 0)
+            return true;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if ((i == 0 || nums[i - 1] == 0)
+                            &&
+                        nums[i] == 0
+                            &&
+                (i == nums.size() - 1 || nums[i + 1] == 0))
+            {
+                n--;
+                i++; // Skip next position since we've placed a flower there
+            }
+        }
+
+        return n <= 0;
+    }
+};
+
+// For first and last index
+// First index value == 0 and i+1 == 0 --> n--
+// Last index value == 0 and i-1 == 0 --> n--
+// FOR i-th position, see if at places i-1 and i+1, the values stored are 0.
