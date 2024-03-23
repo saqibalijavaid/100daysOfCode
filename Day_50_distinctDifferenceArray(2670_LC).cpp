56d089ccd814b78c9ba835ae32fc9d1d974c7f99
Saqib Ali Javaid <saqibalijavaid2@gmail.com>
2024-03-24 00:00:34 +0500

    Update Day_50_distinctDifferenceArray(2670_LC).cpp

--- Day_50_distinctDifferenceArray(2670_LC).cpp
+++ Day_50_distinctDifferenceArray(2670_LC).cpp
@@ -1,1 +1,23 @@
-Hello
+class Solution
+{
+public:
+    vector<int> distinctDifferenceArray(vector<int> &nums)
+    {
+        int n = nums.size();
+        unordered_map<int, int> mp1;
+
+        vector<int> difference(n, 0);
+        for (int i = 0; i < n; i++)
+        {
+            mp1[nums[i]]++;
+            unordered_map<int, int> mp2;
+            for (int j = i + 1; j < n; j++)
+            {
+                mp2[nums[j]]++;
+            }
+            difference[i] = mp1.size() - mp2.size();
+        }
+
+        return difference;
+    }
+};
